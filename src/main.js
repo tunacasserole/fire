ce6fef724a662d1055ee06ba3994002dccda6f82
John Leider <john.j.leider@gmail.com>
2019-03-07 16:49:41 -0600

    style: cleanup

--- src/main.js
+++ src/main.js
@@ -1,15 +1,11 @@
 import Vue from 'vue'
-import './plugins/vuetify'
+import './plugins'
 import App from './App.vue'
-import router from './router'
 import store from './store'
-import axios from 'axios'
-Vue.prototype.$axios = axios
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
   store,
   render: h => h(App)
 }).$mount('#app')
